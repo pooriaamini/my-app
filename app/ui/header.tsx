521f70cc0d4edf0184b82b8b51e6b832674454fa
Pooria <pooriaamini26@gmail.com>
2026-02-21 13:31:23 +0330

    project initialized

---
+++ app/ui/header.tsx
@@ -0,0 +1,18 @@
+import { log } from "node:console";
+import Navigation from "./navigation";
+
+export default function Header() {
+  const date = new Date();
+  const day = date.getDate();
+  const month = date.toLocaleDateString("en-US", { month: "short" });
+  const weekDay = date.toLocaleDateString("en-US", { weekday: "short" });
+  const timeFormat = `${weekDay} / ${day} ${month}`;
+
+  return (
+    <div className="flex justify-between items-center px-7 mt-8">
+      <p className="font-mono hidden md:block text-4xl">Iran/Esfahan</p>
+      <Navigation />
+      <p className="font-sans hidden md:block">{timeFormat}</p>
+    </div>
+  );
+}
---
+++ app/ui/navigation.tsx
@@ -0,0 +1,7 @@
+export default function Navigation(){
+    return(
+        <div className="fixed bottom-6 left-1/2 -translate-x-1/2 md:static md:translate-x-0   bg-black rounded-s-full rounded-e-full w-[25rem] py-1.5">
+            Header navigation 
+        </div>
+    )
+} 
